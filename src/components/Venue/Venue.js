56baf3d74ca64ed9ec1817f57cce10edf218d577
Kunal Aggarwal <kunalagg04@gmail.com>
2019-08-30 00:01:38 +0530

    :-(

--- src/components/Venue/Venue.js
+++ src/components/Venue/Venue.js
@@ -11,14 +11,14 @@ const Venue = () => {
         </div>
         <div className="content">
             <div className="row">
-                <div className="col-6">
+                <div className="col-lg-6">
                 <div id="fmap">
                          
 
                          <iframe width='100%' height='100%' id='mapcanvas' src='https://maps.google.com/maps?q=new%20delhi&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'><div class="zxos8_gm"><a href="https://themesort.com/category/agency-themes">Agency templates at themesort</a></div><div style={{overflow:'hidden'}}><div id='gmap_canvas' style={{height:100+"%" , width:100+"%"}}></div></div><div><small>Powered by <a href="https://www.embedgooglemap.co.uk">Embed Google Map</a></small></div></iframe>
                         </div>
                 </div>
-                <div className="col-6 loc">
+                <div className="col-lg-6 loc">
                     <div>
 
                     <i class="fas fa-map-marker-alt fa-3x"></i>

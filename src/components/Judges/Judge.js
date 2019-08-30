eef3ddfa5781ad5a3c0b2382da20ded61c576d98
Kunal Aggarwal <kunalagg04@gmail.com>
2019-08-31 00:07:49 +0530

    CARAOUSEL

--- src/components/Judges/Judge.js
+++ src/components/Judges/Judge.js
@@ -5,6 +5,8 @@ import Jcard from './Jcard/Jcard';
 
 
 const Judge = () => {
+
+  if(document.body.offsetWidth > 600){
   return (
     <div className="judge">
 
@@ -48,6 +50,47 @@ const Judge = () => {
     
     </div>
   );
+  }
+  else{
+    return(
+      <div className="judge">
+
+
+        
+      <div className="head">
+      Judges
+    </div>
+
+
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+
+      <Jcard/>
+       
+    </div>
+    <div class="carousel-item">
+     <Jcard/>
+    </div>
+    <div class="carousel-item">
+     <Jcard/>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+      </div>
+
+
+    );
+  }
 }
 
 export default Judge;

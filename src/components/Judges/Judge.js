b9c293ae0a4bb79a70337ded934498aebfbe0997
Kunal Aggarwal <kunalagg04@gmail.com>
2019-08-31 00:19:41 +0530

    done

--- src/components/Judges/Judge.js
+++ src/components/Judges/Judge.js
@@ -61,30 +61,15 @@ const Judge = () => {
       Judges
     </div>
 
+<div className="cardi">
 
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
+<Jcard/>
 
-      <Jcard/>
-       
-    </div>
-    <div class="carousel-item">
-     <Jcard/>
-    </div>
-    <div class="carousel-item">
-     <Jcard/>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
+    
+
+
+   
 
       </div>
 
